f5b8c1038b7173da825bee25fec6721c922ed950
Arrravind <aravindking030@gmail.com>
2024-07-18 21:01:13 +0530

    button

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const registerButton = document.getElementById('register-button');
-
-    registerButton.addEventListener('click', () => {
-        alert('Registration form coming soon!');
-    });
 
     particlesJS.load('particles-js', 'particle.json', function() {
         console.log('particles.js config loaded');
