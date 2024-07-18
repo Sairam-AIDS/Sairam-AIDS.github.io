5984826874ee02b0ae5e46b9b47562f7b26c4479
Arrravind <aravindking030@gmail.com>
2024-07-18 19:46:30 +0530

    mid

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const registerButton = document.getElementById('register-button');
+
+    registerButton.addEventListener('click', () => {
+        alert('Registration form coming soon!');
+    });
 
     particlesJS.load('particles-js', 'particle.json', function() {
         console.log('particles.js config loaded');
